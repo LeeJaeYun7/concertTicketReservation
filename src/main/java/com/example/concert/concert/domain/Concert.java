7acb77145f086a2eea1dddc8246967a66273ec56
LeeJongbokZ <s2feeling@naver.com>
2024-11-07 20:19:25 +0900

    콘서트 엔티티 업데이트 & 콘서트 연령 제한 enum 추가

--- src/main/java/com/example/concert/concert/domain/Concert.java
+++ src/main/java/com/example/concert/concert/domain/Concert.java
@@ -1,5 +1,6 @@
 package com.example.concert.concert.domain;
 
+import com.example.concert.concert.enums.ConcertAgeRestriction;
 import com.example.concert.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -18,18 +19,49 @@ public class Concert extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "performance_time")
+    private String performanceTime;
+
+    @Enumerated(EnumType.STRING)
+    private ConcertAgeRestriction ageRestriction;
+
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+
+    @Column(name = "end_at")
+    private LocalDateTime endAt;
+
     @Builder
-    public Concert(String name){
+    public Concert(String name, String genre, String location, String performanceTime, ConcertAgeRestriction ageRestriction, LocalDateTime startAt, LocalDateTime endAt){
         this.name = name;
+        this.genre = genre;
+        this.location = location;
+        this.performanceTime = performanceTime;
+        this.ageRestriction = ageRestriction;
+        this.startAt = startAt;
+        this.endAt = endAt;
         this.setCreatedAt(LocalDateTime.now());
         this.setUpdatedAt(LocalDateTime.now());
     }
 
-    public static Concert of(String name){
+    public static Concert of(String name, String genre, String location, String performanceTime, ConcertAgeRestriction ageRestriction, LocalDateTime startAt, LocalDateTime endAt){
         return Concert.builder()
+                      .genre(genre)
                       .name(name)
+                      .location(location)
+                      .performanceTime(performanceTime)
+                      .ageRestriction(ageRestriction)
+                      .startAt(startAt)
+                      .endAt(endAt)
                       .build();
     }
 }
