9cdc257706c3e72dc43945b91231d0ffa9620030
LeeJongbokZ <s2feeling@naver.com>
2024-11-14 17:07:40 +0900

    remove unused imports

--- src/main/java/com/example/concert/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/concert/reservation/repository/ReservationRepository.java
@@ -3,7 +3,6 @@ package com.example.concert.reservation.repository;
 import com.example.concert.concert.domain.Concert;
 import com.example.concert.reservation.domain.Reservation;
 import jakarta.persistence.LockModeType;
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
