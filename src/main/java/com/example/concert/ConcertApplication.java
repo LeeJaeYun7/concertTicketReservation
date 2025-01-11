33b694b81e530b47250573056b3ab8bded88e20b
LeeJongbokZ <s2feeling@naver.com>
2025-01-11 22:06:46 +0900

    fix multi module config

--- src/main/java/com/example/concert/ConcertApplication.java
+++ src/main/java/com/example/concert/ConcertApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages="global-utils")
+@SpringBootApplication(scanBasePackages="commons")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ConcertApplication {
