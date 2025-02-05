8c473a9bc35e64e28e9a5f58f364b3688a65d514
LeeJongbokZ <s2feeling@naver.com>
2025-02-05 20:37:51 +0900

    트래픽 Aggregator 및 대기열 활성화/비활성화 기능 추가

--- concert-reservation-interfaces/src/main/java/concert/interfaces/waitingqueue/WaitingQueueController.java
+++ concert-reservation-interfaces/src/main/java/concert/interfaces/waitingqueue/WaitingQueueController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,20 +19,38 @@ public class WaitingQueueController {
 
   private final WaitingQueueApplicationService waitingQueueApplicationService;
 
+  @GetMapping("/api/v1/waitingQueue/status")
+  public ResponseEntity<Boolean> getQueueStatus() {
+      boolean isQueueActive = waitingQueueApplicationService.isQueueActive();
+      return ResponseEntity.ok(isQueueActive);
+  }
+
+  @PostMapping("/api/v1/waitingQueue/activate")
+  public ResponseEntity<Void> activateQueue() {
+      waitingQueueApplicationService.activateWaitingQueue();
+      return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/api/v1/waitingQueue/deactivate")
+  public ResponseEntity<Void> deactivateQueue() {
+      waitingQueueApplicationService.deactivateWaitingQueue();
+      return ResponseEntity.ok().build();
+  }
+
   @GetMapping("/api/v1/waitingQueue/token")
   public ResponseEntity<TokenResponse> retrieveToken(@RequestParam(value = "concertId") long concertId, @RequestParam(value = "uuid") String uuid) {
-    TokenVO tokenVO = waitingQueueApplicationService.retrieveToken(concertId, uuid);
-    TokenResponse tokenResponse = TokenResponse.of(tokenVO.getToken());
+      TokenVO tokenVO = waitingQueueApplicationService.retrieveToken(concertId, uuid);
+      TokenResponse tokenResponse = TokenResponse.of(tokenVO.getToken());
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(tokenResponse);
+      return ResponseEntity.status(HttpStatus.CREATED).body(tokenResponse);
   }
 
   @GetMapping("/api/v1/waitingQueue/rank")
   public ResponseEntity<WaitingRankResponse> retrieveWaitingRank(@RequestParam(value = "concertId") long concertId, @RequestParam(value = "token") String token) {
-    String uuid = token.split(":")[1];
-    WaitingRankVO waitingRankVo = waitingQueueApplicationService.retrieveWaitingRank(concertId, uuid);
-    WaitingRankResponse waitingRankResponse = WaitingRankResponse.of(waitingRankVo.getWaitingRank(), waitingRankVo.getStatus());
+      String uuid = token.split(":")[1];
+      WaitingRankVO waitingRankVo = waitingQueueApplicationService.retrieveWaitingRank(concertId, uuid);
+      WaitingRankResponse waitingRankResponse = WaitingRankResponse.of(waitingRankVo.getWaitingRank(), waitingRankVo.getStatus());
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(waitingRankResponse);
+      return ResponseEntity.status(HttpStatus.CREATED).body(waitingRankResponse);
   }
 }
